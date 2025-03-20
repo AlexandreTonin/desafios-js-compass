e57aea4b20a84f96f8373feba8cb00600c7c9e0f
AlexandreTonin <xandetonin@gmail.com>
2025-03-20 11:05:58 -0300

    feat: implementando rota /health-check

--- desafio-1-nodejs/server.js
+++ desafio-1-nodejs/server.js
@@ -1,12 +1,19 @@
-import { createServer } from "node:http"
+import { createServer } from 'node:http'
 
-const hostname = "localhost"
+const hostname = 'localhost'
 const port = 3000
 
 const server = createServer((req, res) => {
+    const { method, url } = req;
+
     res.setHeader('Content-Type', 'application/json')
+
+    if (url === '/health-check' && method === 'GET') {
+        res.writeHead(200)
+        res.end(JSON.stringify({ success: true, timeStamp: new Date().toISOString() }))
+    }
 })
 
-server.listen(port, hostname, ()=>{
+server.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}`)
 })
